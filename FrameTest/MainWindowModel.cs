088cb1af92b12dda5c2587a9b7f4e84a8da991cf
даниил миренский <bucashechca@gmail.com>
2019-05-14 00:39:00 +0300

    ui fix & some comments

--- FrameTest/MainWindowModel.cs
+++ FrameTest/MainWindowModel.cs
@@ -40,24 +40,36 @@ namespace FrameTest
 
         public RelayCommand ButtonClick => new RelayCommand(() =>
         {
+            // make button inactive
             ButtonEnabled = false;
 
             Task.Factory.StartNew(() =>
             {
+                // hard work imitation
                 Thread.Sleep(1000);
 
+                // creating a control
                 var control = new PopupControlModel();
 
+                // getting a view to show in ContentPresenter
                 Message = control.GetVIew();
+                
+                // make text input control visible
                 MessageVisible = Visibility.Visible;
 
+                // run a mainLoop stopping function
                 Result = control.GetInput();
 
+                // hide text input control
                 MessageVisible = Visibility.Collapsed;
+
+                // clear ControlPresenter
                 Message = null;
 
+                // hard work imitation
                 Thread.Sleep(2000);
 
+                // make button active
                 ButtonEnabled = true;
             });
         });
